d2cb2b86b480416a605a61261e03971fcc735054
Saifulgp <abubakar.siddique@kiplepay.com>
2020-07-20 16:11:28 +0800

    magento_v2.3.x updated as API Specifications for Merchant v6.12

--- KiplePay_Plugin_Magento_v2.3.x/KiplePay/Seamless/Controller/Index/Index.php
+++ KiplePay_Plugin_Magento_v2.3.x/KiplePay/Seamless/Controller/Index/Index.php
@@ -2,7 +2,7 @@
 
 namespace KiplePay\Seamless\Controller\Index;
 
-use Magento\Framework\Controller\ResultFactory; 
+use Magento\Framework\Controller\ResultFactory;
 use Magento\Payment\Gateway\ConfigInterface;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
 use Magento\Payment\Gateway\Request\BuilderInterface;
@@ -16,14 +16,14 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
 {
     protected $resultPageFactory;
 
-     /**
-     * @var \Magento\Customer\Model\Session
-     */
+    /**
+    * @var \Magento\Customer\Model\Session
+    */
     protected $_customerSession;
-    
-     /**
-     * @var OrderSender
-     */
+
+    /**
+    * @var OrderSender
+    */
     protected $orderSender;
 
     public function __construct(
@@ -34,13 +34,11 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
         \Magento\Checkout\Model\Session $checkoutSession,
         OrderSender $orderSender,
         \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
-        
-    ) 
-    {
+    ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
         $this->invoiceSender = $invoiceSender;
-        $this->transactionFactory = $transactionFactory;        
+        $this->transactionFactory = $transactionFactory;
         $this->checkoutSession = $checkoutSession;
         $this->orderSender = $orderSender;
         $this->quoteRepository = $quoteRepository;
@@ -58,7 +56,7 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
 
     public function execute()
     {
-        if( isset($_POST['payment_options']) && $_POST['payment_options'] == "1" ) {
+        if (isset($_POST['payment_options']) && $_POST['payment_options'] == "1") {
             // Attempt to store the cart into magento system
             // This function should be execute during KiplePay selection page AFTER the address selection
             // Begin calling Magento API
@@ -74,11 +72,11 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
             $customerSess = $om->create('\Magento\Customer\Model\Session');
             $checkoutHelperData = $om->create('\Magento\Checkout\Helper\Data');
 
-            if( $_POST['current_email'] == ''){
+            if ($_POST['current_email'] == '') {
                 $quote_extra = $this->quoteRepository->getActive($cartData->getId());
                 $_POST['current_email'] = $quote_extra->getBillingAddress()->getEmail();
-            }            
-            
+            }
+
             $customerType = '';
             if ($customerSess->isLoggedIn()) {
                 $customerType = \Magento\Checkout\Model\Type\Onepage::METHOD_CUSTOMER;
@@ -93,21 +91,20 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                 $customerType = $quote->getCheckoutMethod();
             }
 
-            if ( $customerType == \Magento\Checkout\Model\Type\Onepage::METHOD_GUEST) {
-
+            if ($customerType == \Magento\Checkout\Model\Type\Onepage::METHOD_GUEST) {
                 $quote->setCustomerId(null)
                     ->setCustomerEmail($_POST['current_email'])
                     ->setCustomerIsGuest(true)
                     ->setCustomerGroupId(\Magento\Customer\Model\Group::NOT_LOGGED_IN_ID);
             }
 
-            
-            if( $quote ){
+
+            if ($quote) {
                 $cartManagement = $om->create('\Magento\Quote\Model\QuoteManagement');
                 $order = $cartManagement->submit($quote);
 
-                
-                if( $order ){
+
+                if ($order) {
                     $orderArr = [];
                     $orderArr = [
                         'oid' => $order->getId(),
@@ -117,31 +114,28 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                     $order_step2 = $om->create('\Magento\Sales\Model\Order')
                                      ->load($order->getId());
 
-                        $order_step2->setState("pending_payment")->setStatus("pending_payment");
+                    $order_step2->setState("pending_payment")->setStatus("pending_payment");
 
-                                $order_step2->save();
-
+                    $order_step2->save();
                 }
-                
             }
 
             ### Begin to save quote and order in session
             $checkoutSession = $om->create('\Magento\Checkout\Model\Session');
 
             ### initial order created, save their data in session
-            if( $order ){
-                    $checkoutSession->setLastQuoteId($cartData->getId())->setLastSuccessQuoteId($cartData->getId());
-                    $checkoutSession->setLastOrderId($order->getId())
+            if ($order) {
+                $checkoutSession->setLastQuoteId($cartData->getId())->setLastSuccessQuoteId($cartData->getId());
+                $checkoutSession->setLastOrderId($order->getId())
                         ->setLastRealOrderId($order->getIncrementId())
                         ->setLastOrderStatus('pending_payment');
             }
 
             ### When 2nd attempt to make payment but above order create is error then use the session
-            if( !$order ){
+            if (!$order) {
                 $sess_quotedata = $checkoutSession->getData();
 
-                if( isset($sess_quotedata['last_real_order_id']) && $sess_quotedata['last_real_order_id'] != null){
-
+                if (isset($sess_quotedata['last_real_order_id']) && $sess_quotedata['last_real_order_id'] != null) {
                     $lastOId = $sess_quotedata['last_real_order_id'];
 
                     $order = $om->create('\Magento\Sales\Api\Data\OrderInterface');
@@ -157,25 +151,24 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
 
                     ];
                 }
-
             }
-            
+
             $customer_countryid =  $_POST['current_countryid'];
-            
+
             $merchantid = $this->_objectManager->create('KiplePay\Seamless\Helper\Data')->getMerchantID();
             $vkey = $this->_objectManager->create('KiplePay\Seamless\Helper\Data')->getVerifyKey();
             $merchantSecretKey = $this->_objectManager->create('KiplePay\Seamless\Helper\Data')->getSecretKey();
 
             if ($vkey) {
-                $redirectGatewayURL = 'https://uat.kiplepay.com/wcgatewayinit.php';     
+                $redirectGatewayURL = 'https://uat.kiplepay.com/wcgatewayinit.php';
             } else {
-                $redirectGatewayURL = 'https://kiplepay.com/wcgatewayinit.php';    
+                $redirectGatewayURL = 'https://kiplepay.com/wcgatewayinit.php';
             }
 
             $base_url = $this->_objectManager->get('Magento\Store\Model\StoreManagerInterface')->getStore()->getBaseUrl();
 
             ### Make sure amount is always same format
-            $order_amount = number_format(floatval($order->getGrandTotal()),2,'.','');
+            $order_amount = number_format(floatval($order->getGrandTotal()), 2, '.', '');
 
 
             $amountVal = str_replace('.', '', $order_amount);
@@ -190,15 +183,14 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                 'ord_totalamt' => $order_amount,
                 'ord_gstamt' => '0.00',
                 'ord_fcur' => 'MYR',
+                'version' => '2.0',
                 'ord_shipname' => $order->getBillingAddress()->getFirstname()." ".$order->getBillingAddress()->getLastname(),
                 'ord_telephone' => $order->getBillingAddress()->getTelephone(),    // To Do - Change to customer mobile number
                 'ord_email' => $order->getCustomerEmail(),
                 'ord_mercID' => $merchantid,
                 'merchant_hashvalue' => $hashValue,
                 'ord_returnURL' => $base_url.'seamless/'
-            );
-
-            ?>
+            ); ?>
 
                 <html>
                 <head>
@@ -209,8 +201,8 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                         }
                     </SCRIPT>
                 </head>
-                <body onload='return fnSubmit()'> 
-                    <div style="text-align:center; margin-top:10%; font-family:arial; font-size:13px;"> 
+                <body onload='return fnSubmit()'>
+                    <div style="text-align:center; margin-top:10%; font-family:arial; font-size:13px;">
                         <div style="margin-bottom:30px;"><img src="https://kiplepay.com/webroot/img/ajax-loader.gif"></div>
                         <div> Please wait while redirecting to Kiplepay Gateway... </div>
                     </div>
@@ -224,15 +216,14 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                         <input type="hidden" name="ord_telephone" value="<?= $params['ord_telephone'] ?>" />
                         <input type="hidden" name="ord_email" value="<?= $params['ord_email']?>" />
                         <input type="hidden" name="ord_date" value="<?= $params['ord_date']?>" />
+                        <input type="hidden" name="version" value="<?= $params['version']?>" />
                         <input type="hidden" name="merchant_hashvalue" value="<?= $params['merchant_hashvalue']?>" />
                         <input type="hidden" name="ord_returnURL" value="<?= $base_url.'seamless/' ?>" />
                     </form>
                 </body>
         <?php //$this->getResponse()->setBody(json_encode($params));
-            exit;   
-        }
-        else if( isset($_REQUEST['returncode'] ) ) //response from KiplePay 
-        {             
+            exit;
+        } elseif (isset($_REQUEST['returncode'])) { //response from KiplePay
             $P = $_REQUEST;
 
             $findOrderId = explode('-', $P['ord_mercref']);
@@ -248,19 +239,19 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
 
             $amountVal = str_replace('.', '', $P['ord_totalamt']);
             $amountVal = str_replace(',', '', $amountVal);
-        
+
             $returnHashKey = sha1($merchantSecretKey . $merchantid . $P['ord_mercref'] . $amountVal . $P['returncode']);
 
-            if($P['returncode'] == '100') {
-                if($P['ord_key'] == $returnHashKey) {
+            if ($P['returncode'] == '100') {
+                if ($P['ord_key'] == $returnHashKey) {
                     $quoteId = $order->getQuoteId();
                     if ($order->getId() && $order->getState() != 'processing') {
-                        $order->setState('processing',true);
-                        $order->setStatus('processing',true);
+                        $order->setState('processing', true);
+                        $order->setStatus('processing', true);
 
-                        $order->addStatusHistoryComment(__('Response from KiplePay - (Transaction Status : CAPTURED).<br/>You have confirmed the order to the customer.' ))
+                        $order->addStatusHistoryComment(__('Response from KiplePay - (Transaction Status : CAPTURED).<br/>You have confirmed the order to the customer.'))
                               ->setIsCustomerNotified(true);
-                    
+
                         //$payment = $order->getPayment();
                         //$mp_amount = $_POST['amount'];
                         //$mp_txnid = $_POST['tranID'];
@@ -270,53 +261,51 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                     }
 
                     $this->messageManager->addSuccess('Order has been successfully placed!');
-                    
+
                     $this->checkoutSession->setLastQuoteId($quoteId)->setLastSuccessQuoteId($quoteId);
                     $this->checkoutSession->setLastOrderId($order->getId());
-                   
+
                     //page redirect
                     $url_checkoutredirection = 'checkout/onepage/success';
                 } else {
                     $this->messageManager->addError('Key is not valid.');
-                    $order->setState('fraud',true);
-                    $order->setStatus('fraud',true);
+                    $order->setState('fraud', true);
+                    $order->setStatus('fraud', true);
 
                     $history_msg = 'Payment Error: Signature key not match';
 
-                    $order->addStatusHistoryComment(__( $history_msg ))
+                    $order->addStatusHistoryComment(__($history_msg))
                           ->setIsCustomerNotified(false);
 
-                    $this->messageManager->addSuccess('Payment Error: Signature key not match');  
+                    $this->messageManager->addSuccess('Payment Error: Signature key not match');
                     $url_checkoutredirection = 'checkout/cart';
-                }   
-            } else if ($P['returncode'] == 'E1' || $P['returncode'] == 'E2') {
-                $order->setState('canceled',true);
-                $order->setStatus('canceled',true);
+                }
+            } elseif ($P['returncode'] == 'E1' || $P['returncode'] == 'E2') {
+                $order->setState('canceled', true);
+                $order->setStatus('canceled', true);
 
                 $order->addStatusHistoryComment(__('Response from KiplePay - (Transaction Status : FAILED)'))
                         ->setIsCustomerNotified(false);
 
-                $this->messageManager->addSuccess('Fail to complete payment.');        
-                $url_checkoutredirection = 'checkout/cart';   
-
+                $this->messageManager->addSuccess('Fail to complete payment.');
+                $url_checkoutredirection = 'checkout/cart';
             } else {
                 $commentMsg = "Order has been canceled by you";
                 if ($order->getId() && $order->getState() != 'canceled') {
                     $order->registerCancellation($commentMsg)->save();
-                }                    
+                }
 
                 $this->checkoutSession->restoreQuote();
                 $this->messageManager->addError($commentMsg);
 
                 $this->messageManager->addSuccess('Order has been canceled by you!');
-                
-                $url_checkoutredirection = 'checkout/cart';  
+
+                $url_checkoutredirection = 'checkout/cart';
             }
             $order->save();
             $this->_redirect($url_checkoutredirection);
-        }
-        else if( empty($_POST) ){
-           $this->_redirect('/');
+        } elseif (empty($_POST)) {
+            $this->_redirect('/');
         }
     }
 
@@ -340,25 +329,25 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
         curl_close( $ch );
         return;
     }*/
-    
-    public function update_invoice_transaction($order, $payment, $e){ //$a:$order_id, $b:$order, $c:$payment, $d:$mp_amount, $e:$mp_txnid
-        if($order->canInvoice()) {
+
+    public function update_invoice_transaction($order, $payment, $e)
+    { //$a:$order_id, $b:$order, $c:$payment, $d:$mp_amount, $e:$mp_txnid
+        if ($order->canInvoice()) {
             $payment
                     ->setTransactionId($e)
                     ->setShouldCloseParentTransaction(1)
                     ->setIsTransactionClosed(0);
             $invoice = $order->prepareInvoice();
             $invoice->setRequestedCaptureCase(\Magento\Sales\Model\Order\Invoice::CAPTURE_ONLINE);
-                $invoice->register();
-                
-                $transaction = $this->transactionFactory->create();
-                
-                $transaction->addObject($invoice)
+            $invoice->register();
+
+            $transaction = $this->transactionFactory->create();
+
+            $transaction->addObject($invoice)
                 ->addObject($invoice->getOrder())
                 ->save();
-                
         }
-    
+
         try {
             $this->orderSender->send($order);
             $quote = $this->quoteRepository->get($order->getQuoteId())->setIsActive(false);
@@ -366,7 +355,5 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
         } catch (\Exception $e) {
             throw new \Magento\Framework\Exception\LocalizedException(__('We cannot send the new order email.'));
         }
-    
     }
-
 }
